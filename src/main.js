4181f952c273bc81ae4c00dd00f4561b3a95c089
krisGooooo <shenya123@hotmail.com>
2018-05-09 22:08:34 +0800

    home page start

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastClick'// 解决body click延迟300毫秒运行的问题
+import './assets/styles/reset.css'// 解决浏览器显示不统一的问题
+import './assets/styles/border.css'// 解决某些移动端产生的1像素边框问题
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)// 绑定document.body
 
 /* eslint-disable no-new */
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/home/Home.vue'
-import List from '@/pages/list/List.vue'
-
 Vue.use(Router)
 
 export default new Router({
@@ -11,10 +9,6 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
-    }, {
-      path: '/list',
-      name: 'List',
-      component: List
     }
   ]
 })
