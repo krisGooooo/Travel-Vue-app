9f179d8ef153b3b83573b239f8e7cb63e6b829ac
krisGooooo <shenya123@hotmail.com>
2018-05-10 21:47:50 +0800

    change picture

--- src/main.js
+++ src/main.js
@@ -4,12 +4,16 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastClick'// 解决body click延迟300毫秒运行的问题
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import 'styles/reset.css'// 解决浏览器显示不统一的问题
 import 'styles/border.css'// 解决某些移动端产生的1像素边框问题
 import 'styles/iconfont.css'// 引入iconfont
+import 'swiper/dist/css/swiper.css'
+
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)// 绑定document.body
+Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,44 @@
+<template>
+	<div class="wrapper">
+		<swiper :options="swiperOption">
+	    <swiper-slide v-for="item of swiperList" :key="item.id">
+	    	<img class="swiper-img" :src="item.imgUrl">
+	    </swiper-slide>
+	    <div class="swiper-pagination"  slot="pagination"></div>
+	  </swiper>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'HomeSwiper',
+	data () {
+		return {
+			swiperOption: {
+				pagination: '.swiper-pagination',
+				loop: true
+			},
+			swiperList: [{
+				id: '0001',
+				imgUrl: 'http://img1.qunarzz.com/piao/fusion/1805/77/f63bd04dd5319602.jpg_750x200_6ba8e0ca.jpg'
+			},{
+				id: '0002',
+				imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/15/9250dbc86a456302.jpg_750x200_b423f532.jpg'
+			}]
+		}
+	}
+}
+</script>
+
+<style lang="stylus" scoped>
+	.wrapper >>> .swiper-pagination-bullet-active
+			background: #fff
+	.wrapper
+		overflow: hidden
+		width: 100%
+		height: 0
+		padding-bottom: 26.7%
+		background: #eee
+		.swiper-img
+			width: 100%
+</style>
