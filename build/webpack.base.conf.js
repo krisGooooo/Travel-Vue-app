987344c1ad05aca25aa0555a3d76cb69f8506578
krisGooooo <shenya123@hotmail.com>
2018-05-10 20:47:22 +0800

    add header

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -36,6 +36,8 @@ module.exports = {
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
+      'styles': resolve('src/assets/styles'),
+
     }
   },
   module: {
--- src/main.js
+++ src/main.js
@@ -4,8 +4,9 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastClick'// 解决body click延迟300毫秒运行的问题
-import './assets/styles/reset.css'// 解决浏览器显示不统一的问题
-import './assets/styles/border.css'// 解决某些移动端产生的1像素边框问题
+import 'styles/reset.css'// 解决浏览器显示不统一的问题
+import 'styles/border.css'// 解决某些移动端产生的1像素边框问题
+import 'styles/iconfont.css'// 引入iconfont
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)// 绑定document.body
