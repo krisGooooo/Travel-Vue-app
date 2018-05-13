576b3af6bb71839344dca22e7c81d2e15edd6237
krisGooooo <shenya123@hotmail.com>
2018-05-13 22:00:31 +0800

    icons complete

--- src/pages/home/components/icons.vue
+++ src/pages/home/components/icons.vue
@@ -1,103 +1,113 @@
 <template>
 	<div class="icons">
-		<div class="icon">
-			<div class="icon-img">
-				<img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
-			</div>
-
-			<p class="icon-desc">热门景点</p>
-		</div>
-		<div class="icon">
-			<div class="icon-img">
-				<img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
-			</div>
-
-			<p class="icon-desc">热门景点</p>
-		</div>
-		<div class="icon">
-			<div class="icon-img">
-				<img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
-			</div>
-
-			<p class="icon-desc">热门景点</p>
-		</div>
-		<div class="icon">
-			<div class="icon-img">
-				<img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
-			</div>
-
-			<p class="icon-desc">热门景点</p>
-		</div>
-		<div class="icon">
-			<div class="icon-img">
-				<img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
-			</div>
-
-			<p class="icon-desc">热门景点</p>
-		</div>
-		<div class="icon">
-			<div class="icon-img">
-				<img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
-			</div>
-
-			<p class="icon-desc">热门景点</p>
-		</div>
-		<div class="icon">
-			<div class="icon-img">
-				<img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
-			</div>
-
-			<p class="icon-desc">热门景点</p>
-		</div>
-		<div class="icon">
-			<div class="icon-img">
-				<img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
-			</div>
-
-			<p class="icon-desc">热门景点</p>
-		</div>
+		<swiper>
+			<swiper-slide v-for="(page, index) of pages" :key="index">
+				<div class="icon" v-for="item of page" :key="item.id">
+					<div class="icon-img">
+						<img class="icon-img-content" :src="item.imgUrl">
+					</div>
+					<p class="icon-desc">{{item.desc}}</p>
+				</div>
+			</swiper-slide> 
+		</swiper>
 	</div>
 </template>
 
 <script>
 export default {
-	name: 'HomeIcons'
+	name: 'HomeIcons',
+	data () {
+		return {
+			iconList: [{
+				id: '0001',
+				imgUrl: "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+				desc:'景点门票'
+			},{
+				id: '0002',
+				imgUrl: "http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png",
+				desc:'一日游'
+			},{
+				id: '0003',
+				imgUrl: "http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png",
+				desc:'必游榜单'
+			},{
+				id: '0004',
+				imgUrl: "http://img1.qunarzz.com/piao/fusion/1803/76/eb88861d78fb9902.png",
+				desc:'动植物园'
+			},{
+				id: '0005',
+				imgUrl: "http://img1.qunarzz.com/piao/fusion/1803/bd/9f7b9b2b60c1502.png",
+				desc:'踏青赏花'
+			},{
+				id: '0006',
+				imgUrl: "http://img1.qunarzz.com/piao/fusion/1803/b6/37560ece9c62b502.png",
+				desc:'城市观光'
+			},{
+				id: '0007',
+				imgUrl: "http://img1.qunarzz.com/piao/fusion/1803/b1/528a9e80403b8c02.png",
+				desc:'上海欢乐谷'
+			},{
+				id: '0008',
+				imgUrl: "http://img1.qunarzz.com/piao/fusion/1804/ed/cf572be30fc32f02.png",
+				desc:'Q+精选'
+			},{
+				id: '0009',
+				imgUrl: "http://img1.qunarzz.com/piao/fusion/1803/de/f26be47a6bf02a02.png",
+				desc:'上海迪士尼'
+			}]
+		}
+	},
+	computed: {
+		pages () {
+			const pages = []
+			this.iconList.forEach((item, index) => {
+				const page = Math.floor(index / 8)
+				if (!pages[page]) {
+					pages[page] = []
+				}
+				pages[page].push(item)
+			})
+			return pages 
+		}
+	}
 }
 </script>
 
 <style lang="stylus" scoped>
 	@import '~styles/varibles.styl'
-	.icons
-		overflow: hidden
+	@import '~styles/mixins.styl'
+	.icons >>> .swiper-container
 		height: 0
 		padding-bottom: 50%
-		.icon
-			position: relative
-			overflow: hidden
-			float: left
-			width: 25%
-			height: 0
-			padding-bottom: 25%
-			.icon-img
-				position: absolute
-				top: 0
-				left: 0
-				right: 0
-				bottom: .44rem
-				box-sizing: border-box
-				padding: .1rem
-				.icon-img-content
-					display: block
-					margin: 0 auto
-					height: 100%
-			.icon-desc
-				position: absolute
-				left: 0
-				right: 0
-				bottom: 0
-				height: .44rem
-				line-height: .44rem
-				text-align: center
-				color: $darkTextColor
+		
+	.icon
+		position: relative
+		overflow: hidden
+		float: left
+		width: 25%
+		height: 0
+		padding-bottom: 25%
+		.icon-img
+			position: absolute
+			top: 0
+			left: 0
+			right: 0
+			bottom: .44rem
+			box-sizing: border-box
+			padding: .1rem
+			.icon-img-content
+				display: block
+				margin: 0 auto
+				height: 100%
+		.icon-desc
+			position: absolute
+			left: 0
+			right: 0
+			bottom: 0
+			height: .44rem
+			line-height: .44rem
+			text-align: center
+			color: $darkTextColor
+			ellipsis()
 					
 </style>
