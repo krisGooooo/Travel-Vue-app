a63c5f204053182ebfe975bb992d23bec74e5e52
krisGooooo <shenya123@hotmail.com>
2018-05-24 23:30:19 +0800

    add list scroll

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -25,7 +25,9 @@
 					</div>
 				</div>
 			</div>		
-			<div class="area" v-for="(item, key) of cities" :key="key">
+			<div class="area" 
+				v-for="(item, key) of cities" :key="key"
+				:ref="key">
 				<div class="title border-topbottom">
 					{{key}}
 				</div>
@@ -49,10 +51,19 @@ export default {
 	name: 'CityList',
 	props: {
 		hot: Array,
-		cities: Object 
+		cities: Object,
+		letter: String
 	},
 	mounted () {
 		this.scroll = new Bscroll(this.$refs.wrapper)
+	},
+	watch: {
+		letter () {
+			if (this.letter) {
+				const element = this.$refs[this.letter][0]
+				this.scroll.scrollToElement(element)
+			}
+		}
 	}
 }
 </script>
